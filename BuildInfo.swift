c3e25cd44a4a1a6fedbb495df1b066430816d552
GTMoraes <107651397+GTMoraes@users.noreply.github.com>
2026-09-24 05:01:24 -0300

    del lixo

--- BuildInfo.swift
+++
@@ -1,7 +0,0 @@
-// Substituído pelo workflow a cada build (passo "Carimbar a versao no app").
-// Este conteúdo só aparece num build feito fora do GitHub Actions.
-enum BuildInfo {
-    static let commit = "local"
-    static let date   = "sem data"
-    static let run    = "0"
-}
--- VentiladorApp.swift
+++
@@ -1,8 +0,0 @@
-import SwiftUI
-
-@main
-struct VentiladorApp: App {
-    var body: some Scene {
-        WindowGroup { ContentView() }
-    }
-}
